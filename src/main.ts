a815c89b3293ca6418eb4bfb4b8897b48d0198c9
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-10-08 15:08:21 +0700

    Adding morgan as global middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import morgan from 'morgan';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan('dev'))
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
