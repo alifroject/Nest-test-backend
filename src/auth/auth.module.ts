41d164d6a15656ba0478ea415e00621875c82eb5
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-10-09 17:23:49 +0700

    Added password to schema

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,6 +15,6 @@ import { UserModule } from '../user/user.module';
   ],
   controllers: [AuthController],
   providers: [AuthService],
-  exports: [AuthService] // allows use in other modulues here
+  exports: [AuthService] 
 })
 export class AuthModule {}
