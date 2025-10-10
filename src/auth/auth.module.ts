32ad473e5c4320cca39feb139f4c32425dba8cb0
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-10-10 10:19:09 +0700

    auth: Added google as 3rd party-2

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { GoogleStrategy } from '../auth/strategies/google.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '../user/user.module';
 
@@ -14,7 +15,7 @@ import { UserModule } from '../user/user.module';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, GoogleStrategy],
   exports: [AuthService] 
 })
 export class AuthModule {}
